165fd9391e3ee6bd9d426978d5e2f6ba03813a18
Andrem19 <7255591@gmail.com>
2022-09-13 06:00:33 +0100

    Add project files.

---
+++ ConsoleApp6/CustomIpfsFileReader.cs
@@ -0,0 +1,47 @@
+﻿using Ipfs;
+using Ipfs.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class CustomIpfsFileReader
+    {
+		private readonly IpfsClient ipfs;
+
+		internal CustomIpfsFileReader(IpfsClient ipfs)
+		{
+			this.ipfs = ipfs;
+		}
+
+		public async Task<string> ReadAllTextAsync(string path, CancellationToken cancel = default)
+		{
+			await using Stream data = await this.ReadFileAsync(path, cancel);
+			using StreamReader reader = new StreamReader(data);
+			return await reader.ReadToEndAsync();
+		}
+
+		public Task<Stream> ReadFileAsync(string path, CancellationToken cancel = default)
+		{
+			return this.ipfs.PostDownloadAsync("cat", cancel, path);
+		}
+
+		public Task<Stream> ReadFileAsync(string path, long offset, long length = 0, CancellationToken cancel = default)
+		{
+			return this.ipfs.PostDownloadAsync("cat", cancel, path, $"offset={offset}", $"length={length}");
+		}
+
+		public Task<IFileSystemNode> ListFileAsync(string path, CancellationToken cancel = default)
+		{
+			return this.ipfs.FileSystem.ListFileAsync(path, cancel);
+		}
+
+		public Task<Stream> GetAsync(string path, bool compress = false, CancellationToken cancel = default)
+		{
+			return this.ipfs.FileSystem.GetAsync(path, compress, cancel);
+		}
+	}
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,44 @@
+﻿using Ipfs.Http;
+using Ipfs.CoreApi;
+using System.Text;
+using ConsoleApp6;
+
+namespace MainT
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            string path = @"C:\Users\72555\Desktop\index.html";
+            string body = @"<h1>Hello World</h1>";
+            using (FileStream file = new FileStream(path, FileMode.Append))
+            using (StreamWriter stream = new StreamWriter(file))
+                stream.WriteLine(body);
+            var ipfs = new IpfsClient();
+            var cid = await ipfs.FileSystem.AddFileAsync(@"C:\Users\72555\Desktop\index.html"); /*AddTextAsync("I'm so cool");*/
+            Console.WriteLine((string)cid.Id);
+            //var test = await ipfs.FileSystem.AddDirectoryAsync(@"C:\Users\72555\Desktop\Projects\C#\ConsoleApp6\ConsoleApp6\bin\Debug\net6.0");
+            //CustomIpfsFileReader worker = new CustomIpfsFileReader(ipfs);
+            //var res = await worker.ListFileAsync("QmYxtgURbxYd9TuNuMNz5iPRaKvxQZiKEd3sLcGcgHi8Zy");
+            //var res = await worker.ReadAllTextAsync((string)cid.Id);
+            ////string text = StreamToString(res);
+            //Console.WriteLine(res);
+        }
+        public static Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+        public static string StreamToString(Stream stream)
+        {
+            using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
